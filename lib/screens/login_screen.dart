c0427e50858913f651225756972c18d9d2f9804d
tejastn10 <tejastn10@gmail.com>
2021-03-14 12:49:20 +0530

    🌤️ Day 5

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/utils/routes.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String name = "";
+  bool changeBtn = false;
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -17,7 +25,7 @@ class LoginScreen extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.bold,
@@ -27,8 +35,10 @@ class LoginScreen extends StatelessWidget {
               height: 20,
             ),
             Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              padding: const EdgeInsets.symmetric(
+                vertical: 16.0,
+                horizontal: 32.0,
+              ),
               child: Column(
                 children: [
                   TextFormField(
@@ -36,6 +46,11 @@ class LoginScreen extends StatelessWidget {
                       hintText: "Enter User Name",
                       labelText: "Username",
                     ),
+                    onChanged: (val) => {
+                      setState(() => {
+                            name = val,
+                          }),
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -47,19 +62,51 @@ class LoginScreen extends StatelessWidget {
                   SizedBox(
                     height: 40,
                   ),
-                  ElevatedButton(
-                    onPressed: () => {
-                      print("login pressed"),
-                      Navigator.pushNamed(context, Routes.home)
-                    },
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(
-                      minimumSize: Size(
-                        150,
-                        40,
+                  InkWell(
+                    child: AnimatedContainer(
+                      width: changeBtn ? 40 : 150,
+                      height: 40,
+                      alignment: Alignment.center,
+                      duration: Duration(milliseconds: 500),
+                      child: changeBtn
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(changeBtn ? 40 : 5),
+                        color: Colors.deepPurple,
                       ),
                     ),
+                    onTap: () async => {
+                      setState(() => {
+                            changeBtn = true,
+                          }),
+                      await Future.delayed(Duration(milliseconds: 500)),
+                      Navigator.pushNamed(context, Routes.home),
+                    },
                   )
+                  // ElevatedButton(
+                  //   onPressed: () => {
+                  //     print("login pressed"),
+                  //     Navigator.pushNamed(context, Routes.home)
+                  //   },
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize: Size(
+                  //       150,
+                  //       40,
+                  //     ),
+                  //   ),
+                  // )
                 ],
               ),
             )
